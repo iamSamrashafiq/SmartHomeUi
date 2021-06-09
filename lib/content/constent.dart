91fa681af9abb13db5ffe3040f5a3050d512e000
samrashafiq <samrashafique8@gmail.com>
2021-06-09 12:49:36 +0500

    constant file added

---
+++ lib/content/constent.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const App_Color = Color(0xff3796e8);
+const appbar_title = TextStyle(fontSize: 18, color: App_Color,fontWeight: FontWeight.w300,fontFamily: 'Barlow-regular' );
+const smartTask_Style = TextStyle(color:Colors.black,fontFamily: 'Barlow-medium',fontWeight: FontWeight.w600,fontSize: 17);
+const brand_name = TextStyle(color: Colors.grey,fontFamily: 'Barlow-regular',fontSize:15 );
